4fab0a153e14d3f2ef4174e3c598e7d34374b8be
RohitNema11 <rohitnema2006@gmail.com>
2025-06-15 15:44:26 +0530

    project update

--- Summer Bootcamp/React Project/movie-recommendation/src/App.jsx
+++ Summer Bootcamp/React Project/movie-recommendation/src/App.jsx
@@ -2,21 +2,34 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/navBar'
 import Home from './pages/home';
-import WatchList from './pages/watchlist';
+import Watchlist from './pages/watchlist';
 import './App.css'
+import Action from './pages/action';
+import Comedy from './pages/comedy';
+import Thriller from './pages/thriller';
+import Horror from './pages/horror';
+import Family from './pages/family';
+import Animated from './pages/animated';
+import Crime from './pages/crime';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       <NavBar/> 
-      <div className="bg-slate-900 w-full h-screen mt-3"></div>
       <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/watchlist' element={<WatchList/>}/>
-      </Routes>
+        <Route path='/watchlist' element={<Watchlist/>}/>
+        <Route path='/action' element={<Action/>}/>
+        <Route path='/comedy' element={<Comedy/>}/>
+        <Route path='/thriller' element={<Thriller/>}/>
+        <Route path='/horror' element={<Horror/>}/>
+        <Route path='/crime' element={<Crime/>}/>
+        <Route path='/family' element={<Family/>}/>
+        <Route path='/animated' element={<Animated/>}/>
+
+      </Routes> 
     </main>
     </div>
   )
--- Summer Bootcamp/React Project/movie-recommendation/src/components/navBar.jsx
+++ Summer Bootcamp/React Project/movie-recommendation/src/components/navBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
     return (
-        <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between relative m-0">
+        <nav className="bg-black text-white px-6 py-4 flex justify-between relative m-0">
         <div className="text-2xl font-bold">
             <Link to="/">Flickwise</Link>
         </div>
@@ -16,15 +16,19 @@ function NavBar() {
             <div className="nav-link"
                 onClick={() => setIsDropdownOpen(!isDropdownOpen)}
                 style={{ cursor: 'pointer' }}>
-                Explore ▼
-            </div>
+                Genres ▼
+            </div> 
 
                 {/* Dropdown Menu */}
             {isDropdownOpen && (
                 <div className="absolute top-full mt-2 right-0 bg-white text-black shadow-md rounded-md flex flex-col w-40 z-50">
-                    <Link to="/popular" className="nav-link">Popular</Link>
-                    <Link to="/top-rated" className="nav-link">Top Rated</Link>
-                    <Link to="/upcoming" className="nav-link">Upcoming</Link>
+                    <Link to="/action" className="nav-link">Action</Link>
+                    <Link to="/comedy" className="nav-link">Comedy</Link>
+                    <Link to="/thriller" className="nav-link">Thriller</Link>
+                    <Link to="/horror" className="nav-link">Horror</Link>
+                    <Link to="/crime" className="nav-link">Crime</Link>
+                    <Link to="/family" className="nav-link">Family</Link>
+                    <Link to="/animated" className="nav-link">Animated</Link>
                  </div>
             )}
 
